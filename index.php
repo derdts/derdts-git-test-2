bd99f4d942567eb721e309b08a1acad7fd09d5bd
derdts <topunodelmundo@gmail.com>
2024-06-17 15:53:13 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 <body>
     <h1>Hello, world</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi molestiae repellendus eius. Officia sit recusandae nostrum fugit reiciendis iste mollitia provident repellat eius fugiat. Error eos tempore veritatis quae unde.</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
